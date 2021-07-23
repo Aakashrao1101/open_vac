f1a21d722c63dfebc675a24a3a01b11dda02278a
Aakashrao1101 <aakashent2001@gmail.com>
2021-07-23 15:41:54 +0530

    Basic configurations done, robot setup for nav_stack

---
+++ open_vac_description/launch/tp.cpp
@@ -0,0 +1,11 @@
+// A C++ program that compiles and runs another C++ 
+// program
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    system("ls");
+  
+    return 0;
+}
---
+++ open_vac_description/tp.cpp
@@ -0,0 +1,11 @@
+// A C++ program that compiles and runs another C++ 
+// program
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    system("roslaunch open_vac_description display.launch");
+  
+    return 0;
+}
